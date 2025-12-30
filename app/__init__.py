09b3dae52c8523c3c2a37729b0045af673de2e48
dvrkdev <dvrk.devx@gmail.com>
2025-12-30 11:01:46 +0500

    feat: templates created with bootstrap5

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+
+bootstrap5 = Bootstrap5()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates')
+
+    # initialize extensions
+    bootstrap5.init_app(app)
 
     # register blueprints
     from app.routes import auth, main
     app.register_blueprint(auth.bp)
     app.register_blueprint(main.bp)
-    
+
     return app
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
-def home(): return 'home'
+def home(): return render_template('main/home.html')
 
 @bp.route('/create')
-def create_note(): return 'create note'
+def create_note(): return render_template('main/create_note.html')
 
 @bp.route('/read')
-def read_note(): return 'read note'
+def read_note(): return render_template('main/read_note.html')
 
 @bp.route('/update')
-def update_note(): return 'update note'
+def update_note(): return render_template('main/update_note.html')
 
 @bp.route('/delete')
 def delete_note(): return 'delete note'
