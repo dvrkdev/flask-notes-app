78b4b28efe0265fa4e8b03a060377ad1f1180a5d
dvrkdev <dvrk.devx@gmail.com>
2026-01-11 23:09:26 +0500

    feat: note form test

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,13 +1,27 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import current_user, login_required
+
+from app import db
+from app.forms import NoteForm
+from app.models import Note
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
-@bp.route("/")
+@bp.route("/", methods=["POST", "GET"])
 @login_required
 def home():
-    return render_template("main/home.html")
+    notes = Note.query.order_by(Note.created_at.desc()).all()
+    form = NoteForm()
+    if form.validate_on_submit():
+        note = Note(
+            title=form.title.data, content=form.content.data, user_id=current_user.id
+        )
+        db.session.add(note)
+        db.session.commit()
+        flash("Note created successfully.", "success")
+        return redirect(url_for("main.home"))
+    return render_template("main/home.html", notes=notes, form=form)
 
 
 @bp.route("/create")
