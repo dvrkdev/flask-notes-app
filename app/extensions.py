f43e6e91979c8f1d4692326557c6f77d074aa179
dvrkdev <dvrk.devx@gmail.com>
2026-01-14 16:47:47 +0500

    fix: csrf protection

--- app/extensions.py
+++ app/extensions.py
@@ -4,7 +4,7 @@ from flask_babel import Babel
 from flask_ckeditor import CKEditor
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect
 
 babel = Babel()
 login_manager = LoginManager()
